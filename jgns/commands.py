14d6fed6fc852a9ed298b5dab8aba5ed4959811f
Jacob Glueck <swimgiraffe435@gmail.com>
2020-07-04 18:21:13 -0400

    Fix sudo

--- jgns/commands.py
+++ jgns/commands.py
@@ -15,7 +15,7 @@ openssl = Path(jgns_deps.openssl) / "bin/openssl"
 pvcreate = Path(jgns_deps.lvm2) / "bin/pvcreate"
 rsync = Path(jgns_deps.rsync) / "bin/rsync"
 stow = Path(jgns_deps.stow) / "bin/stow"
-sudo = Path(jgns_deps.coreutils) / "bin/sudo"
+sudo = Path(jgns_deps.sudo) / "bin/sudo"
 swapon = Path(jgns_deps.utillinux) / "bin/swapon"
 vgcreate = Path(jgns_deps.lvm2) / "bin/vgcreate"
 
